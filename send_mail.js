cc247366bc93e1dcfebec5aa3c7c47ea31db57db
Mani Karan <msinghster@gmail.com>
2017-12-23 13:08:11 +0530

    email changes

--- send_mail.js
+++ send_mail.js
@@ -3,17 +3,12 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'msinghster@gmail.com',
-    pass: 'Tigers007@'
+    user: 'mtrt@wew.com',
+    pass: '323@'
   }
 });
 
-var mailOptions = {
-  from: 'msinghster@gmail.com',
-  to: 'mani@creativeindia.net.in',
-  subject: 'Sending Email using Node.js',
-  html: '<h1>Welcome</h1><p>That was easy!</p>'
-};
+
 
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
